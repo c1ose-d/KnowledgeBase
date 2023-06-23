05116efaecb65107da86ebe974041a151ace2944
Torsen52 <torsen52@gmail.com>
2023-06-23 16:23:05 +0300

    v0.0.0.2-build

--- KnowledgeBase/Cards/AddCard.xaml.cs
+++ KnowledgeBase/Cards/AddCard.xaml.cs
@@ -1,42 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿namespace KnowledgeBase.Cards;
 
-namespace KnowledgeBase.Cards
+public partial class AddCard : Window
 {
-    /// <summary>
-    /// Interaction logic for AddCard.xaml
-    /// </summary>
-    public partial class AddCard : Window
+    public AddCard()
     {
-        public AddCard()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
 
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            using (KnowledgeBaseContext db = new())
-            {
-                db.Add(new Solution { Title = NameCard.Text, Description = DescriptionCard.Text, TagId = Convert.ToInt32(/*я не ебу, как взять этот kind*/) });
-                db.SaveChanges(); //сукаблять
-                Close();
-            }
-        }
-
-        private void Cancel_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
+        using KnowledgeBaseContext db = new();
+        Solution_Tag.ItemsSource = db.Tags.ToList();
     }
+
+    private void Save_Click(object sender, RoutedEventArgs e)
+    {
+        using (KnowledgeBaseContext db = new())
+        {
+            db.Add(new Solution
+            {
+                Title = Solution_Title.Text,
+                Description = Solution_Description.Text,
+                TagId = ((Tag)Solution_Tag.SelectedItem).Id
+            });
+            db.SaveChanges();
+        }
+
+        Close();
+    }
+
+    private void Cancel_Click(object sender, RoutedEventArgs e) =>
+        Close();
 }
--- KnowledgeBase/Resources/Pages/SolutionsPage.xaml.cs
+++ KnowledgeBase/Resources/Pages/SolutionsPage.xaml.cs
@@ -2,8 +2,13 @@
 
 public partial class SolutionsPage : Page
 {
-    public SolutionsPage() =>
+    public SolutionsPage()
+    {
         InitializeComponent();
+
+        using KnowledgeBaseContext db = new();
+        Solutions.ItemsSource = db.Solutions.Include(s => s.Tag).ToList();
+    }
 
     private void Page_Loaded(object sender, RoutedEventArgs e) { }
 }
