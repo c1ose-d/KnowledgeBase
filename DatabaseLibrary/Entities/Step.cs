7181b459db6ec10d1c2ed8c3112654621ffd2211
Pelmen <Pelmen@DESKTOP-FCPDJ33>
2023-06-23 16:34:19 +0300

    я не понимаю

--- DatabaseLibrary/Entities/Step.cs
+++ DatabaseLibrary/Entities/Step.cs
@@ -1,12 +1,9 @@
 ﻿namespace DatabaseLibrary.Entities;
 
-public class Solution
+public class Step
 {
     public int Id { get; set; }
     public string Title { get; set; } = null!;
     public string Description { get; set; } = null!;
-    public int TagId { get; set; }
-    public List<Tag> Tags { get; set; } = new();
-
-    public Tag? Tag { get; set; }
+    public Solution? Solution { get; set; }
 }
