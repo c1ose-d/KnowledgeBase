39a5f34cc513b75a7ef550686beddef462e32c76
Nedzhuk <jopkakota322@outlook.com>
2023-06-28 22:25:32 +0300

    окно просмотра карточки

---
+++ KnowledgeBase/Tags/ViewSolution.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KnowledgeBase.Tags
+{
+    /// <summary>
+    /// Interaction logic for ViewSolution.xaml
+    /// </summary>
+    public partial class ViewSolution : Window
+    {
+        public ViewSolution()
+        {
+            InitializeComponent();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
